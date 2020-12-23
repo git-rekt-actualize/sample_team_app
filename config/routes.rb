3820e29af7054794d6a3cb013d426bb3bcb92411
nadia <nadiaagahozo@gmail.com>
2020-12-23 14:29:52 -0600

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/pages" => "pages#index"
-<<<<<<< HEAD
-
     get "things" => "things#index"
-=======
     get "/widgets" => "widgets#index"
     get "/hale" => "hale#index"
->>>>>>> 7d0c8ecd67b0e75fb58b15fc69b276e7c4f5f8b5
   end
 end
