b5fac82e62c9d224822ecb9d920cb7dfbe071539
Remington <remingtonnberg@gmail.com>
2020-12-23 12:47:57 -0600

    added my name to the json

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,9 +1,6 @@
 class Api::PagesController < ApplicationController
   def index
-<<<<<<< HEAD
     render json: { message: "ReMiNgToN" }
-=======
-    render json: {message: "Wendy's here!"}
->>>>>>> cd12292801c4e670841b1242575e313e7e463e3d
+    render json: { message: "Wendy's here!" }
   end
 end
